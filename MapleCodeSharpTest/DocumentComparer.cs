de4b0f8b3ee9d9383aeafc90dc32ab47240012e8
Zhenwei Wu <wuzhenwei35@gmail.com>
2019-11-30 17:29:41 -0500

    Add DocumentComparer for testing.

---
+++ MapleCodeSharpTest/DocumentComparer.cs
@@ -0,0 +1,148 @@
+﻿using MapleCodeSharp.Reader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapleCodeSharpTest
+{
+    class DocumentComparer
+    {
+        private struct TypePair
+        {
+            public readonly int A, B;
+            public TypePair(int a, int b) { A = a; B = b; }
+        }
+
+        private HashSet<TypePair> _equalTypes = new HashSet<TypePair>();
+
+        private List<Node> _compareNodeA = new List<Node>();
+        private List<Node> _compareNodeB = new List<Node>();
+
+        private Dictionary<Node, int> _nodeSequence = new Dictionary<Node, int>();
+        private int _nodeCount = 0;
+
+        private bool CompareDoc(Document a, Document b)
+        {
+            var nodesA = a.AllNodes.ToArray();
+            var nodesB = b.AllNodes.ToArray();
+            if (nodesA.Length != nodesB.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < nodesA.Length; ++i)
+            {
+                if (!Compare(nodesA[i], nodesB[i]))
+                {
+                    return false;
+                }
+            }
+
+            for (int i = 0; i < _compareNodeA.Count; ++i)
+            {
+                if (!_nodeSequence.TryGetValue(_compareNodeA[i], out var ia))
+                {
+                    return false;
+                }
+                if (!_nodeSequence.TryGetValue(_compareNodeB[i], out var ib))
+                {
+                    return false;
+                }
+                if (ia != ib) return false;
+            }
+
+            return true;
+        }
+
+        private bool Compare(Node a, Node b)
+        {
+            var ta = a.NodeType;
+            var tb = b.NodeType;
+            if (!_equalTypes.Contains(new TypePair(ta.Index, tb.Index)))
+            {
+                if (!Compare(ta, tb))
+                {
+                    return false;
+                }
+                _equalTypes.Add(new TypePair(ta.Index, tb.Index));
+            }
+
+            var ga = a.ReadGenericArgs();
+            var gb = b.ReadGenericArgs();
+            for (int i = 0; i < ga.Length; ++i)
+            {
+                if (ga[i] != gb[i]) return false;
+            }
+
+            var aa = a.ReadArguments();
+            var ab = b.ReadArguments();
+            for (int i = 0; i < aa.Length; ++i)
+            {
+                if (!Compare(aa[i], ab[i])) return false;
+            }
+
+            var ca = a.Children.ToArray();
+            var cb = b.Children.ToArray();
+            if (ca.Length != cb.Length) return false;
+            for (int i = 0; i < ca.Length; ++i)
+            {
+                if (!Compare(ca[i], cb[i])) return false;
+            }
+
+            _nodeSequence[a] = _nodeCount;
+            _nodeSequence[b] = _nodeCount;
+            _nodeCount += 1;
+
+            return true;
+        }
+
+        private bool Compare(NodeType a, NodeType b)
+        {
+            if (a.Name != b.Name) return false;
+            if (a.GenericArgCount != b.GenericArgCount) return false;
+            if (a.HasChildren != b.HasChildren) return false;
+            if (a.ArgumentTypes.Count != b.ArgumentTypes.Count) return false;
+            for (int i = 0; i < a.ArgumentTypes.Count; ++i)
+            {
+                if (a.ArgumentTypes[i] != b.ArgumentTypes[i]) return false;
+            }
+            return true;
+        }
+
+        private bool Compare(NodeArgument a, NodeArgument b)
+        {
+            if (a.TypeClass != b.TypeClass) return false;
+            switch (a.TypeClass)
+            {
+                case NodeArgumentTypeClass.Unsigned:
+                    return a.GetUnsigned() == b.GetUnsigned();
+                case NodeArgumentTypeClass.Signed:
+                    return a.GetSigned() == b.GetSigned();
+                case NodeArgumentTypeClass.Float:
+                    return a.GetFloat() == b.GetFloat();
+                case NodeArgumentTypeClass.String:
+                    return a.GetString() == b.GetString();
+                case NodeArgumentTypeClass.Data:
+                    return Enumerable.SequenceEqual(a.GetData(), b.GetData());
+                case NodeArgumentTypeClass.Ref:
+                {
+                    _compareNodeA.Add(a.GetNode());
+                    _compareNodeB.Add(b.GetNode());
+                    return true;
+                }
+                case NodeArgumentTypeClass.RefField:
+                {
+                    _compareNodeA.Add(a.GetNode());
+                    _compareNodeB.Add(b.GetNode());
+                    return a.GetField() == b.GetField();
+                }
+                default: return false;
+            }
+        }
+
+        public static bool Compare(Document a, Document b)
+        {
+            return new DocumentComparer().CompareDoc(a, b);
+        }
+    }
+}
--- MapleCodeSharpTest/DocumentReadTest.cs
+++ MapleCodeSharpTest/DocumentReadTest.cs
@@ -227,43 +227,48 @@ namespace MapleCodeSharpTest
             }
         }
 
+        private Document CreateElementSizeDocument(int str, int type, int node, int data)
+        {
+            var builder = new DocumentBuilder(str, type, node, data);
+
+            var t1 = builder.TypeTable.AddType("t1", 1, true, NodeArgumentTypeValues.U16, NodeArgumentTypeValues.REF);
+            var t2 = builder.TypeTable.AddType("t2", 0, false, NodeArgumentTypeValues.STR, NodeArgumentTypeValues.DAT);
+
+            var r = ArgumentBuilder.NewRefArg();
+            builder.NodeSection.WriteNode(t1, new[] { "t" }, 0u, r);
+            var c1 = builder.NodeSection.StartChildrenList();
+            var n2 = builder.NodeSection.WriteNode(t2, "string", new byte[] { 1, 2, 3 });
+            builder.NodeSection.FixReference(r, n2);
+            builder.NodeSection.EndChildrenList(c1);
+
+            return ReadDocument(builder.Generate());
+        }
+
+        private void CheckElementSizeDocument(Document doc)
+        {
+            var nodes = doc.AllNodes;
+            var n1 = Assert.Single(nodes);
+
+            Assert.Equal("t1", n1.NodeType.Name);
+            Assert.Equal(new[] { "t" }, n1.ReadGenericArgs());
+            Assert.True(n1.NodeType.HasChildren);
+            var n2 = Assert.Single(n1.Children);
+            var n1a = n1.ReadArguments();
+            Assert.Equal(0u, n1a[0].GetUnsigned());
+            Assert.Equal(n2, n1a[1].GetNode());
+
+            Assert.Equal("t2", n2.NodeType.Name);
+            Assert.Empty(n2.ReadGenericArgs());
+            Assert.False(n2.NodeType.HasChildren);
+            var n2a = n2.ReadArguments();
+            Assert.Equal("string", n2a[0].GetString());
+            Assert.Equal(new byte[] { 1, 2, 3 }, n2a[1].GetData());
+        }
+
         private void TestElementSize(int str, int type, int node, int data)
         {
-            Document doc;
-            {
-                var builder = new DocumentBuilder(str, type, node, data);
-
-                var t1 = builder.TypeTable.AddType("t1", 1, true, NodeArgumentTypeValues.U16, NodeArgumentTypeValues.REF);
-                var t2 = builder.TypeTable.AddType("t2", 0, false, NodeArgumentTypeValues.STR, NodeArgumentTypeValues.DAT);
-
-                var r = ArgumentBuilder.NewRefArg();
-                builder.NodeSection.WriteNode(t1, new[] { "t" }, 0u, r);
-                var c1 = builder.NodeSection.StartChildrenList();
-                var n2 = builder.NodeSection.WriteNode(t2, "string", new byte[] { 1, 2, 3 });
-                builder.NodeSection.FixReference(r, n2);
-                builder.NodeSection.EndChildrenList(c1);
-
-                doc = ReadDocument(builder.Generate());
-            }
-            {
-                var nodes = doc.AllNodes;
-                var n1 = Assert.Single(nodes);
-
-                Assert.Equal("t1", n1.NodeType.Name);
-                Assert.Equal(new[] { "t" }, n1.ReadGenericArgs());
-                Assert.True(n1.NodeType.HasChildren);
-                var n2 = Assert.Single(n1.Children);
-                var n1a = n1.ReadArguments();
-                Assert.Equal(0u, n1a[0].GetUnsigned());
-                Assert.Equal(n2, n1a[1].GetNode());
-
-                Assert.Equal("t2", n2.NodeType.Name);
-                Assert.Empty(n2.ReadGenericArgs());
-                Assert.False(n2.NodeType.HasChildren);
-                var n2a = n2.ReadArguments();
-                Assert.Equal("string", n2a[0].GetString());
-                Assert.Equal(new byte[] { 1, 2, 3 }, n2a[1].GetData());
-            }
+            var doc = CreateElementSizeDocument(str, type, node, data);
+            CheckElementSizeDocument(doc);
         }
 
         [Fact]
@@ -279,6 +284,17 @@ namespace MapleCodeSharpTest
             TestElementSize(1, 1, 1, 4);
             TestElementSize(2, 2, 2, 2);
             TestElementSize(4, 4, 4, 4);
+        }
+
+        [Fact]
+        public void CompareLongTable()
+        {
+            var doc1111 = CreateElementSizeDocument(1, 1, 1, 1);
+            var doc2222 = CreateElementSizeDocument(2, 2, 2, 2);
+            var doc1124 = CreateElementSizeDocument(1, 1, 2, 4);
+            Assert.True(DocumentComparer.Compare(doc1111, doc2222));
+            Assert.True(DocumentComparer.Compare(doc1111, doc1124));
+            Assert.True(DocumentComparer.Compare(doc2222, doc1124));
         }
     }
 }
